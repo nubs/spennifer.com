094286e0102b448729f4ecd5081ded192f6cb280
Spencer Rinehart <anubis@overthemonkey.com>
2008-06-02 20:51:50 -0400

    Removed pam from the ceremony page and added 2 more pictures of the church instead

--- ceremony.php
+++ ceremony.php
@@ -5,15 +5,17 @@
 		<td><img src="images/ceremony/gbumc2.jpg" /></td>
 		<td><img src="images/ceremony/gbumc3.jpg" /></td>
 	</tr>
-</table>
-<p style="width: 80%; margin: 0 auto;">The ceremony will be held at <a target="_blank" href="www.greatbridgeumc.org">Great Bridge United Methodist Church</a>, Spencer and Jennifer's home church in Chesapeake, Virginia.  The Rineharts and the McClures have been active members of GBUMC for quite some time and could not have asked for a better church family.  Jennifer has worked as the part-time secretary for three summers, and during that time developed a close bond with the church staff.  Consequently, there are no two people Spencer and Jennifer would rather have perform the ceremony than Rev. Kesner and Rev. Gable.</p>
-<table style="width: 80%; margin: 10px auto; text-align: center; font-size: 20px;">
 	<tr>
-		<td><img src="images/ceremony/danny.jpg" /></td>
-		<td><img src="images/ceremony/pam.jpg" /></td>
+		<td colspan="3">The ceremony will be held at <a target="_blank" href="http://www.greatbridgeumc.org">Great Bridge United Methodist Church</a>, Spencer and Jennifer's home church in Chesapeake, Virginia.  The Rineharts and the McClures have been active members of GBUMC for quite some time and could not have asked for a better church family.  Jennifer has worked as the part-time secretary for three summers, and during that time developed a close bond with the church staff.  Consequently, there are no two people Spencer and Jennifer would rather have perform the ceremony than Rev. Kesner.</td>
 	</tr>
 	<tr>
+		<td><img src="images/ceremony/gbumc4.jpg" /></td>
+		<td><img src="images/ceremony/danny.jpg" /></td>
+		<td><img src="images/ceremony/gbumc5.jpg" /></td>
+	</tr>
+	<tr>
+		<td></td>
 		<td>Rev. Danny Kesner</td>
-		<td>Rev. Pam Gable</td>
+		<td></td>
 	</tr>
 </table>
