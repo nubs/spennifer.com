80d86d6645aeddd6cd8d5de4708706b5ee7fa246
Spencer Rinehart <anubis@overthemonkey.com>
2008-06-04 07:26:57 -0400

    Fixed text

--- justmarried.php
+++ justmarried.php
@@ -1,4 +1,4 @@
-After their wedding, Spencer and Jennifer will be living together in their new townhome that they bought in January.  Since they became homeowners, they have been working on furnishing the house and they are well on their way to calling it a home.
+Spencer and Jennifer recently purchased a townhouse in the Great Bridge area of Chesapeake.  The townhouse is an end unit with two bedrooms, two & half baths, brand new kitchen appliances, a deck, and a see-though fireplace!  Spencer will be moving into the new house on the 25th of January and Jennifer will join him in August after the wedding.  Since they became homeowners, they have been working on furnishing the house and they are well on their way to calling it a home.
 <table style="text-align: center; margin: 0 auto;">
 	<tr>
 		<td><img src="images/justmarried/house_1.jpg"></td>
