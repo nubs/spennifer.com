973ba9cb9b9f021f708cffc1a94c4921d9346384
Spencer Rinehart <anubis@overthemonkey.com>
2008-06-04 09:35:39 -0400

    Photo gallery

--- photos.php
+++ photos.php
@@ -1,1 +1,17 @@
 <div><img src="images/photos/photos_text.jpg" /></div>
+<div class="image"><a href="images/photos/02.jpg" target="_blank"><img src="small/images/photos/02.jpg" /></a></div>
+<div class="image"><a href="images/photos/05.jpg" target="_blank"><img src="small/images/photos/05.jpg" /></a></div>
+<div class="image"><a href="images/photos/09.jpg" target="_blank"><img src="small/images/photos/09.jpg" /></a></div>
+<div class="image"><a href="images/photos/10.jpg" target="_blank"><img src="small/images/photos/10.jpg" /></a></div>
+<div class="image"><a href="images/photos/13.jpg" target="_blank"><img src="small/images/photos/13.jpg" /></a></div>
+<div class="image"><a href="images/photos/08.jpg" target="_blank"><img src="small/images/photos/08.jpg" /></a></div>
+<div class="image"><a href="images/photos/01.jpg" target="_blank"><img src="small/images/photos/01.jpg" /></a></div>
+<div class="image"><a href="images/photos/03.jpg" target="_blank"><img src="small/images/photos/03.jpg" /></a></div>
+<div class="image"><a href="images/photos/04.jpg" target="_blank"><img src="small/images/photos/04.jpg" /></a></div>
+<div class="image"><a href="images/photos/06.jpg" target="_blank"><img src="small/images/photos/06.jpg" /></a></div>
+<div class="image"><a href="images/photos/07.jpg" target="_blank"><img src="small/images/photos/07.jpg" /></a></div>
+<div class="image"><a href="images/photos/11.jpg" target="_blank"><img src="small/images/photos/11.jpg" /></a></div>
+<div class="image"><a href="images/photos/12.jpg" target="_blank"><img src="small/images/photos/12.jpg" /></a></div>
+<div class="image"><a href="images/photos/14.jpg" target="_blank"><img src="small/images/photos/14.jpg" /></a></div>
+<div class="image"><a href="images/photos/15.jpg" target="_blank"><img src="small/images/photos/15.jpg" /></a></div>
+<div class="image"><a href="images/photos/16.jpg" target="_blank"><img src="small/images/photos/16.jpg" /></a></div>
