2e6a3e63f4bf477d81d330be63d0507c3444934f
Spencer Rinehart <anubis@overthemonkey.com>
2008-06-04 08:32:14 -0400

    Changed 3 pics of house to the collage of pics.

--- justmarried.php
+++ justmarried.php
@@ -1,8 +1,2 @@
 <p>Spencer and Jennifer recently purchased a townhouse in the Great Bridge area of Chesapeake.  The townhouse is an end unit with two bedrooms, two & half baths, brand new kitchen appliances, a deck, and a see-though fireplace!  Spencer will be moving into the new house on the 25th of January and Jennifer will join him in August after the wedding.  Since they became homeowners, they have been working on furnishing the house and they are well on their way to calling it a home.</p>
-<table style="text-align: center; margin: 0 auto;">
-	<tr>
-		<td><img src="images/justmarried/house_1.jpg"></td>
-		<td><img src="images/justmarried/house_2.jpg"></td>
-		<td><img src="images/justmarried/house_3.jpg"></td>
-	</tr>
-</table>
+<img src="images/justmarried/house.jpg">
