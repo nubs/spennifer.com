d09f43ef11f280edcbda3ea5979a72bc0bdb891b
Spencer Rinehart <anubis@overthemonkey.com>
2008-06-04 07:55:52 -0400

    Changed registry from belk to target

--- registries.php
+++ registries.php
@@ -2,7 +2,7 @@
 <table style="margin: 10px auto 0 auto; text-align: center;" cellpadding="0" cellspacing="5px">
 	<tr>
 		<td width="140px"><img style="float: right;" src="images/registries/flower.jpg" /></td>
-		<td colspan="2" width="279px"><a target="_blank" href="http://www.belk.com/giftregistry/gift_registry_view.jsp?registry_code=ek1%3A45b7927a4fe5bcec8765de38efe490348b0c80a71885815416846383517c3ebd&bmUID=1207535786093"><img src="images/registries/belk.jpg" /></a></td>
+		<td colspan="2" width="279px"><a target="_blank" href="http://www.target.com/gp/registry/registry.html/ref=cm_cw_sr_1/601-9332016-6975324?ie=UTF8&type=wedding&id=2I1P8SSGGPU3H&jsebd=1"><img src="images/registries/target.jpg" /></a></td>
 		<td width="139px"><img style="float: left;" src="images/registries/flower.jpg" /></td>
 	</tr>
 </table>
