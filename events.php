1229703a728474420d4bfe46247ab9e6e2fa9539
Spencer Rinehart <anubis@overthemonkey.com>
2011-04-08 10:46:23 -0400

    Adding new pictures.

--- events.php
+++ events.php
@@ -1,2 +1,8 @@
 <div style="margin-bottom: 10px;"><img src="images/events/upcoming_events.jpg" /></div>
-Check back soon to find out more information on the rehearsal dinner at the Shifting Sands Banquet Hall in Dam Neck and any upcoming bridal showers.
+The rehearsal dinner will take place on August 7th at the Shifting Sands Fireside Room on the Dam Neck Naval Base at 7:00pm.<br>It is a beautiful room overlooking the beach!  Directions to Shifting Sands can be found on our <a href="index.php?guests">map</a>.
+<table style="width: 80%; margin: 0 auto; text-align: center;">
+	<tr>
+		<td><img src="images/events/rehearsal_1.jpg" /></td>
+		<td><img src="images/events/rehearsal_2.jpg" /></td>
+	</tr>
+</table>
