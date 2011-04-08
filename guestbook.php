bdd3f04bb7930abb993f6de34002ad7f85acac5e
Spencer Rinehart <anubis@overthemonkey.com>
2011-04-08 10:48:31 -0400

    Make guestbook static.

--- guestbook.php
+++ guestbook.php
@@ -1,31 +1,17 @@
-<?php
-$db = mysql_connect('anubis.homelinux.com', 'anubis', '1015184') or die('mysql connect error');
-mysql_select_db('wedding') or die('mysql selectdb error');
-if(@$_POST['message'] && @$_POST['from'])
-{
-	mysql_query("INSERT INTO guestbook (date, who, message) VALUES(NOW(), '" . mysql_real_escape_string($_POST['from']) . "', '" . mysql_real_escape_string($_POST['message']) . "')") or die(mysql_errno() . ': ' . mysql_error());
-	echo "Thank you for signing our guestbook!";
-}
-?>
-<form method="post" action="index.php?guestbook">
-<table style="margin: 10px auto 0 auto; text-align: center;" cellpadding="0" cellspacing="5px">
-	<tr>
-		<th style="text-align: right">From:</th>
-		<td style="text-align: left;"><input type="text" name="from"></td>
-	</tr>
-	<tr>
-		<th style="text-align: right">Message:</th>
-		<td style="text-align: left"><textarea name="message" cols="50" rows="7"></textarea></td>
-	</tr>
-	<tr>
-		<td colspan="2" style="text-align: right;"><input type="submit" name="send" value="Send"></td>
-	</tr>
-</table>
-</form>
-<?php
-$messages_result = mysql_query("SELECT * FROM guestbook ORDER BY date DESC");
-while($messages_row = mysql_fetch_array($messages_result))
-{
-	echo "<div><i>{$messages_row['who']} signed the guestbook at {$messages_row['date']}</i><br>{$messages_row['message']}</div><hr style=\"width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;\">";
-}
-?>
+<div><i>Angie signed the guestbook at 2008-06-27 09:50:13</i><br>The website is wonderful! Your pictures really capture the love you two share.  I am really looking forward to August 8th and beyond!  I love you both and feel very blessed to call you my children.  :)  Mom </div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Rosemary Johnson signed the guestbook at 2008-06-26 08:18:29</i><br>I loved the slideshow.  It brought tears to my eyes.  It\'s been a long time since 4th grade.  Bill and I will be there to celebrate with the both of you.  Much love to you both.</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Sean signed the guestbook at 2008-06-12 23:43:22</i><br>Love the website.  I knew you two were creative, but this is really special.  Much love!</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Dale aka  Mom signed the guestbook at 2008-06-10 14:30:24</i><br>I love the website!!!  Best wishes to a wonderful and loving couple.  May you have many happy years together!!</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Betty & Chip signed the guestbook at 2008-06-07 22:06:58</i><br>Great website - love the pictures.  Best wishes for a long and happy marriage.</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Clair and Ginny signed the guestbook at 2008-06-06 19:06:18</i><br>What a nice website!  Clair and I know you will have a long and happy marriage starting your honeymoon off at Cove Haven - we stayed there 40 years ago!</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Keri McClure signed the guestbook at 2008-06-05 15:42:38</i><br>It\'s almost here. Finally!!! So exciting. Guess I better get my speech all ready for the shower.</div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+<div><i>Spennifer signed the guestbook at 2008-06-02 20:50:25</i><br>Thank you to all who have visited this website!  We look forward to having you share in our special day! :) </div>
+<hr style="width: 40%; margin: 10px auto; border: 0px; height: 1px; color: white; background-color: white;">
+
