05e4e087d4f6da7373c94c262254c4387d8dba32
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-02-17 23:01:39 -0500

    Add some initial content (sans images).

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var bower = require('gulp-bower');
 var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 var paths = {
   css: 'src/styles/**/*.less',
-  fonts: 'bower_components/font-awesome/fonts/**/*'
+  fonts: 'bower_components/font-awesome/fonts/**/*',
+  js: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/js/carousel.js', 'src/scripts/**/*.js']
 };
 
 gulp.task('bower', function() {
@@ -23,8 +26,17 @@ gulp.task('fonts', function() {
   return gulp.src(paths.fonts)
     .pipe(gulp.dest('public/fonts'));
 });
-gulp.task('watch', function() {
-  gulp.watch(paths.css, ['css']);
+
+gulp.task('js', function() {
+  return gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('default', ['bower', 'css', 'fonts']);
+gulp.task('watch', function() {
+  gulp.watch(paths.css, ['css']);
+  gulp.watch(paths.js, ['js']);
+});
+
+gulp.task('default', ['bower', 'css', 'fonts', 'js']);
